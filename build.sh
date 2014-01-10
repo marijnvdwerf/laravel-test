285a246bd9ef0d6f307920ff6786729e18de22f0
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2014-01-10 15:38:15 +0100

    Rewrite script downloading

--- build.sh
+++ build.sh
@@ -1,37 +1,38 @@
 #!/bin/sh
 
-# Check composer installation
-if ! type /usr/local/bin/composer > /dev/null 2>&1; then
+# Check brew installation
+if ! type /usr/local/bin/brew > /dev/null 2>&1; then
+    echo 'Installing homebrew'
+    ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go/install)"
+fi
 
-    # Check brew installation
-    if ! type /usr/local/bin/brew > /dev/null 2>&1; then
-        echo 'Installing homebrew'
-        ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go/install)"
-    fi
-
-    echo 'Installing composer using Homebrew'
-    /usr/local/bin/brew tap josegonzalez/homebrew-php
-    /usr/local/bin/brew install josegonzalez/php/composer
+# Check wget installation
+if ! type /usr/local/bin/wget > /dev/null 2>&1; then
+    echo 'Installing wget'
+    /usr/local/bin/brew install wget
 fi
 
 # Check composer installation
-if ! type /usr/local/bin/phpunit > /dev/null 2>&1; then
+if ! type /usr/local/bin/composer > /dev/null 2>&1; then
+    echo 'Installing composer'
+    /usr/local/bin/wget https://getcomposer.org/installer
+    /usr/bin/php installer
+    /bin/chmod +x composer.phar
+    /bin/mv composer.phar /usr/local/bin/composer
+    /usr/local/bin/composer --version
+fi
 
-    # Check wget installation
-    if ! type /usr/local/bin/wget > /dev/null 2>&1; then
-        echo 'Installing wget'
-        /usr/local/bin/brew install wget
-    fi
-    
+# Check phpunit installation
+if ! type /usr/local/bin/phpunit > /dev/null 2>&1; then
     echo 'Installing phpunit'
     /usr/local/bin/wget https://phar.phpunit.de/phpunit.phar
     /bin/chmod +x phpunit.phar
     /bin/mv phpunit.phar /usr/local/bin/phpunit
-    /usr/local/bin/phpunitphpunit --version
+    /usr/local/bin/phpunit --version
 fi
 
 echo 'Fetching dependencies'
 /usr/local/bin/composer install
 
 echo 'Run unit tests'
-/usr/local/php5/bin/phpunit
+/usr/local/bin/phpunit
